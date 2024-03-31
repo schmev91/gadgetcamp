f2450c66333f00bdc16af3dd67b37477f674a6a6
schmev <86550672+schmev91@users.noreply.github.com>
2024-03-31 21:40:36 +0700

    done home banner

--- src/js/controller/home.ctrl.js
+++ src/js/controller/home.ctrl.js
@@ -1,5 +1,25 @@
 export function HomeCtrl($scope, $rootScope, $http) {
   $rootScope.page = "home";
+  // banner data
+  $scope.banners = [
+    {
+      link: "",
+      active: true,
+      image: "./src/img/banner/iphonexs-banner.jpg",
+    },
+    {
+      link: "",
+      image: "./src/img/banner/macbookpro-banner.jpg",
+    },
+    {
+      link: "",
+      image: "./src/img/banner/furniture-banner.jpg",
+    },
+    {
+      link: "",
+      image: "./src/img/banner/womens_fashion-banner.jpg",
+    },
+  ];
 
   if (!$rootScope.categoryList) {
     $http.get("https://dummyjson.com/products/categories").then((res) => {
@@ -11,6 +31,11 @@ export function HomeCtrl($scope, $rootScope, $http) {
     $http.get("https://dummyjson.com/products?limit=100").then((res) => {
       $rootScope.products = res.data.products;
       $scope.viewProducts = $rootScope.products.slice(0, 12);
+      // console.log(
+      //   $rootScope.products.filter(({ category }) => {
+      //     if (category == "automotive") return true;
+      //   })
+      // );
     });
   } else {
     $scope.viewProducts = $rootScope.products.slice(0, 12);
